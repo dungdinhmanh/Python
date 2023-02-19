1a461d4212e5d5c8c81895a7cba402b91dbd1da5
dungdinhmanh <dungdinhmanh0209@gmail.com>
2023-02-19 21:16:12 +0700

    edited

--- just a python file.py
+++ just a python file.py
@@ -1,13 +1,12 @@
 start=int(input('Nhập thời gian bắt đầu: '))
 end=int(input('Nhập thời gian kết thúc: '))
 t=0
-if end<14:
+if start<14 and end<14:
     t=end-start
     t*=10000
     print('Số tiền phải trả khi gửi trẻ từ', start,'h đến', end,'h là:', t)
-elif end>14:
-    t=end-start
-    t*=15000
+elif start>14 and end>14:
+    t=(end-start)*15000
     print('Số tiền phải trả khi gửi trẻ từ', start,'h đến', end,'h là:', t)
 elif start<14 and end>14:
     t= ((14-start)*10000)+((end-14)*15000)
