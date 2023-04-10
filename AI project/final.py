406db7b8ee9359112898384090bc1853bcb19691
dungdinhmanh <dungdinhmanh0209@gmail.com>
2023-04-10 22:59:32 +0700

    added new file and some changes

--- AI project/final.py
+++ AI project/final.py
@@ -1,5 +1,5 @@
-import speech_recognition
-import pyttsx3
+import speech_recognition, pyaudio
+import pyttsx3, os
 from datetime import date, datetime
 micro=speech_recognition.Recognizer()
 spk=pyttsx3.init()
@@ -8,23 +8,23 @@ date=date.today()
 time=datetime.now()
 while True:
     with speech_recognition.Microphone() as mic:
-        print("Haru: I'm listening")
+        print("Haru: Tôi đang nghe")
         audio=micro.listen(mic)
     print('Haru: ...')
     try:
-        user=micro.recognize_google(audio)
+        user=micro.recognize_google(audio,language="vi-VI")
     except:
         user=''
     print('You: '+user)
 
     if user=='':
-        ai="I can't hear you, please try again!"
+        ai="Tôi không thể nghe thấy bạn nói, hãy thử lại"
     elif 'hello' in user:
         ai='Hello, User'
     elif 'today' in user:
-        ai=('Today is: '+ date.strftime("%d - %m - %Y"))
+        ai=('Hôm nay là '+ date.strftime("ngày %d tháng %m năm %Y"))
     elif 'time' in user:
-        ai=('Current time is: '+ time.strftime("%H hours %M minutes %S seconds"))
+        ai=('Current time is: '+ time.strftime("%H giờ %M phút %S giây"))
     elif 'bye' in user:
         ai='Goodbye, User.'
         print(ai)
@@ -32,7 +32,7 @@ while True:
         spk.runAndWait()
         break
     else:
-        ai="I can't understand your request, please try again!"
+        ai="Tôi không hiểu yêu cầu của bạn, hãy thử lại!"
     print(ai)
 
     spk.say(ai)
