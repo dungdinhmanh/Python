2e6130f64caf97704586cc9e6efe6c820d2abf19
dungdinhmanh <dungdinhmanh0209@gmail.com>
2023-04-15 09:43:37 +0700

    added some change

--- decuong ck2/bai2.py
+++ decuong ck2/bai2.py
@@ -9,13 +9,13 @@ def num_in_str(n):      #Hàm in các số có trong xâu ra màn hình
         if i>='0' and i<='9':
             s.append(i)
     print('Các số có trong xâu vừa nhập là:', ", ".join(s))
-def char_sequence(n):       #Hàm đếm số chữ cái Tiếng Anh trong xâu
+def char_sequence(n):   #Hàm đếm số chữ cái Tiếng Anh trong xâu
     char=0
     for i in n:
         if (i>='A' and i<='Z') or (i>='a' and i<='z'):
             char+=1
     print('Có', char, 'chữ cái tiếng Anh trong xâu')
-def str_symmetry(n):        #Kiểm tra xâu đối xứng
+def str_symmetry(n):    #Kiểm tra xâu đối xứng
     s=''
     for i in n:
         s=i+n
@@ -23,3 +23,9 @@ def str_symmetry(n):        #Kiểm tra xâu đối xứng
         print('Xâu đối xứng')
     else:
         print('Xâu không đối xứng')
+
+s=input('Nhập 1 xâu: ')
+str_reverse(s)
+num_in_str(s)
+char_sequence(s)
+str_symmetry(s)
