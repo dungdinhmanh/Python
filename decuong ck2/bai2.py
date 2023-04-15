d12c82ae9e2b48c3a39ba64d5ea2ba7dbf77e46d
dungdinhmanh <dungdinhmanh0209@gmail.com>
2023-04-15 09:41:23 +0700

    added bài 2

---
+++ decuong ck2/bai2.py
@@ -0,0 +1,25 @@
+def str_reverse(n):     #Hàm đảo ngược xâu
+    s=''
+    for i in n:
+        s=i+n
+    print('Xâu được đảo ngược là:', s)
+def num_in_str(n):      #Hàm in các số có trong xâu ra màn hình
+    s=[]
+    for i in n:
+        if i>='0' and i<='9':
+            s.append(i)
+    print('Các số có trong xâu vừa nhập là:', ", ".join(s))
+def char_sequence(n):       #Hàm đếm số chữ cái Tiếng Anh trong xâu
+    char=0
+    for i in n:
+        if (i>='A' and i<='Z') or (i>='a' and i<='z'):
+            char+=1
+    print('Có', char, 'chữ cái tiếng Anh trong xâu')
+def str_symmetry(n):        #Kiểm tra xâu đối xứng
+    s=''
+    for i in n:
+        s=i+n
+    if n==s:
+        print('Xâu đối xứng')
+    else:
+        print('Xâu không đối xứng')
